55341e379e61ef06ced1c5ddea44274f3131b7dc
Michal Wojtysiak <mkwojtysiak@gmail.com>
2015-10-25 21:26:26 +0100

    Task 4 completed

--- Health Check/ViewController.swift
+++ Health Check/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //Uncomment for Task 4:
-        //navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Compose, target: self, action: Selector("consentButtonTapped:"))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Compose, target: self, action: Selector("consentButtonTapped:"))
         
         //Remove for bonus task
         hanoiButton.enabled = false
@@ -43,7 +43,42 @@ class ViewController: UIViewController {
     @IBAction func consentButtonTapped(sender: AnyObject) {
         //Task 4: 
         //Create consent document (ORKConsentDocument), use it to build  consent step (ORKVisualConsentStep) and consent review step (ORKORKConsentReviewStep). Use those steps to create ORKOrderedTask and display it using ORKTaskViewController
+
+        let document = ORKConsentDocument()
         
+        //Document Sections
+        let welcomeSection = ORKConsentSection(type: .Overview)
+        welcomeSection.title = "Welcome"
+        welcomeSection.summary = "Information about study"
+        welcomeSection.content = "This app will support you on daily basis with measuring your contidion. It is not mandatory to use this app. You can always contact team if you neeed support."
+        let dataGatherSection = ORKConsentSection(type: .DataGathering)
+        let privacySection = ORKConsentSection(type: .Privacy)
+        let dataUseSection = ORKConsentSection(type: .DataUse)
+        let timeCommitmentSection = ORKConsentSection(type: .TimeCommitment)
+        let studySurveySection = ORKConsentSection(type: .StudySurvey)
+        let studyTasksSection = ORKConsentSection(type: .StudyTasks)
+        let withdrawingSection = ORKConsentSection(type: .Withdrawing)
+        
+        document.sections = [welcomeSection, dataGatherSection, privacySection, dataUseSection, timeCommitmentSection, studySurveySection, studyTasksSection, withdrawingSection]
+        document.signaturePageTitle = "Consent"
+        
+        //Document Signature
+        let signature = ORKConsentSignature()
+        signature.requiresName = true
+        signature.requiresSignatureImage = true
+        document.signatures = [signature]
+        
+        //Steps Creation
+        let visualConsentStep = ORKVisualConsentStep(identifier: "visualConsent", document: document)
+        let reviewStep = ORKConsentReviewStep(identifier: "reviewConsent", signature: signature, inDocument: document)
+        reviewStep.text = "Please provide your first and last name. Data privacy is guaranteed"
+        reviewStep.reasonForConsent = "Please confirm that you understand and agree to terms stated in consent"
+        
+        let consentTask = ORKOrderedTask(identifier: "consentTask", steps: [visualConsentStep, reviewStep])
+
+        let taskVC = ORKTaskViewController(task: consentTask, taskRunUUID: nil)
+        taskVC.delegate = self
+        presentViewController(taskVC, animated: true, completion: nil)
     }
     
     @IBAction func hanoiButtonTapped(sender: AnyObject) {
@@ -52,7 +87,7 @@ class ViewController: UIViewController {
 }
 
 
-// Task 3: 
+// Task 3:
 //Catch results of task in ORKTaskViewControllerDelegate's didFinishWithReason method. Implement delegate method in extension - Swifty way ;)
 
 extension ViewController: ORKTaskViewControllerDelegate{
@@ -79,6 +114,7 @@ extension ViewController: ORKTaskViewControllerDelegate{
         }
         
         dismissViewControllerAnimated(true) { [unowned self]() -> Void in
+            guard finalResults?.count > 0 else {return}
             let resultsVC = UINavigationController(rootViewController: ResultsTableViewController(results: finalResults))
             self.presentViewController(resultsVC, animated: true, completion: nil)
         }
